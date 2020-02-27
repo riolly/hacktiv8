54b941b92c103842f0f048e9411b53dc423aad9d
riolly <albert.r.cahyantara@gmail.com>
2020-02-27 21:31:38 +0700

    Tugas 2

---
+++ Week-3/exercise-02-array-1.js
@@ -0,0 +1,14 @@
+var balikString = function(str){
+    // Buat variable untuk menyimpan string yang sudah dibalik
+    var balikStr = "";
+    // Buat loop sejumlah panjang string.
+    for (var i = 1; i <= str.length; i++){
+        // Tambakah setiap karakter mulai dari yang paling belakang.
+        balikStr += str[str.length-i]
+    }
+    // Keluarkan output hasil string yg dibalik
+    return balikStr;
+}
+
+// Tampilkan "Hello World!" terbalik dengan menggunakan fungsi
+console.log(balikString("Hello Wordl!"));
