392bcf0e9ea6ca35224646abc053a03e588a86d5
riolly <albert.r.cahyantara@gmail.com>
2020-02-27 20:24:47 +0700

    Tanggalan yang sesungguhnya

--- Week-2/exercise-04.js
+++ Week-2/exercise-04.js
@@ -1,6 +1,42 @@
-var tanggal = 24;
-var bulan = 4;
-var tahun = 2020;
+var tanggal = 29;
+var bulan = 2;
+var tahun = 2021;
+
+// Maaf instruktur saya gak lihat commentnya disoal :)
+// Ini sekalian saya buatkan tanggalan yang sungguhan.
+
+// Kita tentukan jumlah hari (Tanggal) untuk setiap bulannya
+// Bulan Genap jumlah hari = 30, kecuali Februari; Bulan ganjil jumlah hari = 31;
+
+var maxTanggal = "";
+// Jika bulan bukan Februari
+if (bulan !== 2) {
+    // Jika bulan genap maka jumlah hari = 30
+    if (bulan % 2 == 0) {
+        maxTanggal = 30;
+    // Jika bulan ganjil maka jumlah hari = 31
+    } else {
+        maxTanggal = 31;
+    }
+// Jika bulan Februari
+} else {
+    // Jika tahun kabisat maka jumlah hari = 29
+    if ((tahun % 4 == 0 && tahun % 100 != 0) ||(tahun % 4 == 0 && tahun % 100 == 0 && tahun % 400 == 0)){
+        maxTanggal = 29;
+    // Jika tahun merupakan tahun bukan kabisat maka jumlah hari = 28
+    } else {
+        maxTanggal = 28;
+    }
+}
+
+// Diperiksa apakah tanggal sesuai dengan jumlah hari dalam bulan tersebut
+if (tanggal < 1 || tanggal > maxTanggal) {
+    tanggal = "Tanggal Invalid"
+}
+
+if (tahun < 1900 || tahun > 2200) {
+    tanggal = "Tahun Invalid"
+}
 
 switch (bulan){
     case 1:
@@ -43,5 +79,6 @@ switch (bulan){
         bulan = "Invalid";
         break;
 }
+
 
 console.log(`${tanggal} ${bulan} ${tahun}`);
