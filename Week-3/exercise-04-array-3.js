71747885fad5ad076222aaf1ca7cd5d393ba64ea
riolly <albert.r.cahyantara@gmail.com>
2020-02-28 15:27:24 +0700

    Menampilkan nama bulan

--- Week-3/exercise-04-array-3.js
+++ Week-3/exercise-04-array-3.js
@@ -1,4 +1,3 @@
-
 var input = ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"];
 
 function dataHandling2(arr){
@@ -9,11 +8,56 @@ function dataHandling2(arr){
     console.log(arr);
 
     // ambil tanggal pada input index 3, split string pada '/', ambil indeks ke 1 untuk ambil bulannya saja.
-    var bulan = arr[3].split("/")[1];
+    var bulan = (arr[3].split("/")[1]);
     console.log(bulan);
 
-    
+    // tampilkan nama bulan dgn memanggil fungsi untuk mengganti dari angkar ke literal
+    console.log(gantiBulan(bulan));
 }
 
+// Men
+function gantiBulan(month){
+    switch (Number(month)){
+        case 1:
+            month = "Januari";
+            break;
+        case 2:
+            month = "Februari";
+            break;
+        case 3:
+            month = "Maret";
+            break;
+        case 4:
+            month = "April";
+            break;
+        case 5:
+            month = "Mei";
+            break;
+        case 6:
+            month = "Juni";
+            break;
+        case 7:
+            month = "Juli";
+            break;
+        case 8:
+            month = "Agustus";
+            break;
+        case 9:
+            month = "September";
+            break;
+        case 10:
+            month = "Oktober";
+            break;
+        case 11:
+            month = "November";
+            break;
+        case 12:
+            month = "Desember";
+            break;
+        default :
+            month = "Bulan Invalid";
+    }
+    return month;
+}
 
 dataHandling2(input);
