892fcfc693d4b08416354beac639045804bea527
riolly <albert.r.cahyantara@gmail.com>
2020-02-29 22:48:42 +0700

    Buat file dan soal

---
+++ Week-3/exercise-15-kelompok-15.js
@@ -0,0 +1,9 @@
+function groupAnimals(animals) {
+// you can only write your code here!
+}
+
+// TEST CASES
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
+// [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
+// [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda', 'kancil'], ['unta'] ]
