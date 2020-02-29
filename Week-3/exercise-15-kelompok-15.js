0d48dc2cd669489b83ddd45a3135fa99651986d3
riolly <albert.r.cahyantara@gmail.com>
2020-03-01 00:35:01 +0700

    Mengelompokan hewan berdasarkan alfabet

--- Week-3/exercise-15-kelompok-15.js
+++ Week-3/exercise-15-kelompok-15.js
@@ -1,6 +1,33 @@
+/**
+ * Interasi sejumlah array
+ * buat array untuk menyimpan vokal yang sudah ada
+ * cek apakah element punya karakter yang sama dengan element2 di array vokal.
+ * Jika sudah ada, masukkan
+ * JIka belum buat array baru dan simpal vokalt
+ * @param {string[]} animals array of string
+ */
 function groupAnimals(animals) {
-// you can only write your code here!
+    var sortedAnimals = [];
+    var huruf = [];
+    for (var i = 0; i < animals.length; i++) {
+        for (var j = 0; j <= huruf.length; j++) {
+            if (animals[i][0] == huruf[j]){
+                sortedAnimals[j].push(animals[i]);
+                // console.log(`Kategori lama ${animals[i]}`);
+                break;
+            } else if (j == huruf.length) {
+                sortedAnimals.push([animals[i]]);
+                huruf.push(animals[i][0]);
+                // console.log(`Kategori baru ${animals[i]}`);
+                break;
+            }
+        }
+        // console.log(sortedAnimals);
+        // console.log(huruf);
+    }
+    return sortedAnimals.sort();
 }
+
 
 // TEST CASES
 console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
