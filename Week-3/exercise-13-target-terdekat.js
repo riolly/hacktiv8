34018aade71ad3aad59f209b0da0eff3d79bc353
riolly <albert.r.cahyantara@gmail.com>
2020-02-29 19:52:44 +0700

    Skeleton Code

---
+++ Week-3/exercise-13-target-terdekat.js
@@ -0,0 +1,10 @@
+function targetTerdekat(arr) {
+// you can only write your code here!
+}
+
+// TEST CASES
+console.log(targetTerdekat([' ', ' ', 'o', ' ', ' ', 'x', ' ', 'x'])); // 3
+console.log(targetTerdekat(['o', ' ', ' ', ' ', 'x', 'x', 'x'])); // 4
+console.log(targetTerdekat(['x', ' ', ' ', ' ', 'x', 'x', 'o', ' '])); // 1
+console.log(targetTerdekat([' ', ' ', 'o', ' '])); // 0
+console.log(targetTerdekat([' ', 'o', ' ', 'x', 'x', ' ', ' ', 'x'])); // 2
