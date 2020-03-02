1a1bf0e4b8d394552f9599946a02fe0967e25d2e
riolly <albert.r.cahyantara@gmail.com>
2020-03-02 22:44:45 +0700

    Efficient ascending number sorting

--- Week-3/sort.js
+++ Week-3/sort.js
@@ -1,23 +1,15 @@
-var nilai = [10, 5, 8, 6, 9, 2, 6, 24, 7, 1, 2];
+var nilai = [10, 5, 100, 8, 6, 9, 2, 6, 24, 7, 1, 2];
 
 function sortNumber(array) {
     for (var i = 0; i < array.length-1; i++){
-    var less, more = 0;
-        for (var j = 0; j < array.length-i-1; j++){
-            if (array[j] > array[j+1]){
-                less = array[j+1];
-                more = array[j];
-                // console.log(`J:${j}. ${array[j]} lebih besar ${array[j+1]}`);
-            } else {
-                less = array[j];
-                more = array[j+1];
-                // console.log(`J:${j}. ${array[j]} lebih kecil ${array[j+1]}`);
+        var buffer = 0;
+        for (var j = 0; j < array.length-i-1; j++){     // Permutasi
+            if (array[j] > array[j+1]){                 // Jika kanan lebih besar 
+                buffer = array[j+1];    // ambil yang besar
+                array[j+1] = array[j];  // geser yang kecil
+                array[j] = buffer;      // masukkan kembali yang besar
             }
-            array[j] = less;
-            array[j+1] = more;
-            // console.log(array);
         }
-        // console.log(i+'\n');
     }
     return array;
 }
