d811aa24c382a8926c922aca7d51db34de4ec0f6
riolly <albert.r.cahyantara@gmail.com>
2020-03-02 23:24:30 +0700

    Added option for descending

--- Week-3/sort.js
+++ Week-3/sort.js
@@ -1,17 +1,33 @@
-var nilai = [10, 5, 100, 8, 6, 9, 2, 6, 24, 7, 1, 2];
-
-function sortNumber(array) {
+/**
+ * This function sort array of number ascending or descending.
+ * @example
+ * var input = [...];
+ * sortNumber(input, "ascending");
+ * sortNumber(input, "descending");
+ * @param {number[]} array number
+ * @param {string} order ascending | descending 
+ */
+function sortNumber(array, order = 'ascending') {
     for (var i = 0; i < array.length-1; i++){
         var buffer = 0;
-        for (var j = 0; j < array.length-i-1; j++){     // Permutasi
-            if (array[j] > array[j+1]){                 // Jika kanan lebih besar 
-                buffer = array[j+1];    // ambil yang besar
-                array[j+1] = array[j];  // geser yang kecil
-                array[j] = buffer;      // masukkan kembali yang besar
+        for (var j = 0; j < array.length-i-1; j++){         // Permutasi
+            if (order == "ascending") {
+                if (array[j] > array[j+1]){                 // Jika kanan lebih besar 
+                    buffer = array[j+1];    // ambil yang besar
+                    array[j+1] = array[j];  // geser yang kecil
+                    array[j] = buffer;      // masukkan kembali yang besar
+                }
+            } else if (order == "descending") {
+                if (array[j] < array[j+1]){                 // Jika kiri lebih besar 
+                    buffer = array[j+1];    // ambil yang kecil
+                    array[j+1] = array[j];  // geser yang besar
+                    array[j] = buffer;      // masukkan kembali yang kecil
+                }
             }
         }
     }
     return array;
 }
 
-console.log(sortNumber(nilai));
+var nilai = [10, 5, 100, 8, 6, 9, 2.4, 6, 24, 7, 1, 2];
+console.log(sortNumber(nilai, 'descending'));
