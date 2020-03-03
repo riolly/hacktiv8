eb4320d4a754889275513a8e7cbbab6a3f8f826a
riolly <albert.r.cahyantara@gmail.com>
2020-03-04 02:00:15 +0700

    Diganti huruf setelahnya

--- Week-4/exercise-05-ganti-huruf.js
+++ Week-4/exercise-05-ganti-huruf.js
@@ -1,5 +1,14 @@
 function ubahHuruf(kata) {
-    // you can only write your code here!
+    const alfabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a'];
+    let kataBaru = '';
+    for (var i = 0; i < kata.length; i++) {
+        let j = 0;
+        while (kata[i] != alfabet[j]) {
+            j++;
+        }
+        kataBaru += alfabet[j+1];
+    }
+    return kataBaru;
 }
 
 // TEST CASES
