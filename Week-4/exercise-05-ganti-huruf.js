49477ec8f11ad280426a3fb104bec408726f987b
riolly <albert.r.cahyantara@gmail.com>
2020-03-04 01:50:35 +0700

    Soal

---
+++ Week-4/exercise-05-ganti-huruf.js
@@ -0,0 +1,10 @@
+function ubahHuruf(kata) {
+    // you can only write your code here!
+}
+
+// TEST CASES
+console.log(ubahHuruf('wow')); // xpx
+console.log(ubahHuruf('developer')); // efwfmpqfs
+console.log(ubahHuruf('javascript')); // kbwbtdsjqu
+console.log(ubahHuruf('keren')); // lfsfo
+console.log(ubahHuruf('semangat')); // tfnbohbu
