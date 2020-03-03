3800158d56fa8abc80f90b8bbd9ed7bbe784018e
riolly <albert.r.cahyantara@gmail.com>
2020-03-03 20:50:35 +0700

    Soal

---
+++ Week-4/exercise-02-fpb.js
@@ -0,0 +1,10 @@
+function fpb(angka1, angka2) {
+    // you can only write your code here!
+}
+
+// TEST CASES
+console.log(fpb(12, 16)); // 4
+console.log(fpb(50, 40)); // 10
+console.log(fpb(22, 99)); // 11
+console.log(fpb(24, 36)); // 12
+console.log(fpb(17, 23)); // 1
