3b4c860c25d23047be659c6699af5d167210eb59
riolly <albert.r.cahyantara@gmail.com>
2020-03-03 20:58:21 +0700

    Find factor

--- Week-4/exercise-02-fpb.js
+++ Week-4/exercise-02-fpb.js
@@ -1,10 +1,21 @@
 function fpb(angka1, angka2) {
-    // you can only write your code here!
+    let faktorAngka1 = cariFaktor(angka1);
+    let faktorAngka2 = cariFaktor(angka2);
+}
+
+function cariFaktor(bilangan){
+    let faktor = [];
+    for(var i = 1; i < bilangan -1; i++){
+        if (bilangan % (i+1) == 0){
+            faktor.push(i+1);
+        }
+    }
+    return faktor;
 }
 
 // TEST CASES
-console.log(fpb(12, 16)); // 4
-console.log(fpb(50, 40)); // 10
-console.log(fpb(22, 99)); // 11
-console.log(fpb(24, 36)); // 12
-console.log(fpb(17, 23)); // 1
+// console.log(fpb(12, 16)); // 4
+// console.log(fpb(50, 40)); // 10
+// console.log(fpb(22, 99)); // 11
+// console.log(fpb(24, 36)); // 12
+// console.log(fpb(17, 23)); // 1
