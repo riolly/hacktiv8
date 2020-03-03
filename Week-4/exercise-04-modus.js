7ff225dec4a2e7130d4878a69397a2213ebce7b1
riolly <albert.r.cahyantara@gmail.com>
2020-03-04 01:40:47 +0700

    Deteksi jumlah semunya sama atau angka semuanya sama

--- Week-4/exercise-04-modus.js
+++ Week-4/exercise-04-modus.js
@@ -1,7 +1,6 @@
 function cariModus(arr) {
     let number = [arr[0]];
     let count = [1];
-
     for (var i = 1; i < arr.length; i++){
         let j = 0;
         while (true){
@@ -9,7 +8,7 @@ function cariModus(arr) {
                 count[j] = count[j]+1;
                 break;
             } else {
-                if (j < count.length-1) {       // Jika berbeda dan bukan pengecekan terakhir maka cek angka berikutnya
+                if (j < count.length-1) {     // Jika berbeda dan bukan pengecekan terakhir maka cek angka berikutnya
                     j++;
                 } else {
                     number[j+1] = arr[i];     // Jika berbeda dan tidak nemu juga angka yang sama sampai diujung maka masukkan angka ke dalam lib.
@@ -25,21 +24,24 @@ function cariModus(arr) {
     } else {
         let terbanyak = count[0];
         let angkaTerbanyak = number[0];
+        let change = false;
         for (var i = 1; i < count.length; i++){
-            if (count[i] > terbanyak) {
-                terbanyak = count[i];
-                angkaTerbanyak = number[i];
+            if (count[i] != terbanyak){
+                change = true;
+                if (count[i] > terbanyak) {
+                    terbanyak = count[i];
+                    angkaTerbanyak = number[i];
+                }
             }
         }
-        return angkaTerbanyak;
+        if (change == true) {
+            return angkaTerbanyak;
+        } else {
+            return -1;
+        }
     }
-    // console.log(number);
-    // console.log(count);
-    // console.log(terbanyak);
-    // console.log(angkaTerbanyak);
 }
 
-// cariModus([10, 4, 6, 10, 4, 5, 10, 5, 7]);
 
 // TEST CASES
 console.log(cariModus([10, 4, 5, 2, 4])); // 4
@@ -48,4 +50,4 @@ console.log(cariModus([10, 3, 1, 2, 5])); // -1
 console.log(cariModus([1, 2, 3, 3, 4, 5])); // 3
 console.log(cariModus([7, 7, 7, 7, 7])); // -1
 
-console.log(cariModus([10, 3, 1, 2, 5, 3, 2, 3]));
+// console.log(cariModus([10, 3, 1, 2, 2, 5, 5])); // 2
