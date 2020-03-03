03b2eee213d85548f7cd7e4c557969ce52f766f0
riolly <albert.r.cahyantara@gmail.com>
2020-03-03 19:57:39 +0700

    soal

---
+++ Week-4/exercise-01-angka-prime.js
@@ -0,0 +1,10 @@
+function angkaPrima(angka) {
+    ;
+}
+
+// TEST CASES
+console.log(angkaPrima(3)); // true
+console.log(angkaPrima(7)); // true
+console.log(angkaPrima(6)); // false
+console.log(angkaPrima(23)); // true
+console.log(angkaPrima(33)); // false
