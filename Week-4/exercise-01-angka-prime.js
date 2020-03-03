6b628b367d5fc3eaef4925aa40d76e56e4371489
riolly <albert.r.cahyantara@gmail.com>
2020-03-03 20:45:48 +0700

    prime number checker

--- Week-4/exercise-01-angka-prime.js
+++ Week-4/exercise-01-angka-prime.js
@@ -1,5 +1,13 @@
 function angkaPrima(angka) {
-    ;
+    if (angka <= 1) {
+        return false;
+    }
+    for (var i = 1; i < angka-1; i++){   
+        if (angka % (i+1) == 0){
+            return false;
+        }
+    }
+    return true;
 }
 
 // TEST CASES
@@ -8,3 +16,8 @@ console.log(angkaPrima(7)); // true
 console.log(angkaPrima(6)); // false
 console.log(angkaPrima(23)); // true
 console.log(angkaPrima(33)); // false
+
+// Test case -5 - 50
+// for (var i = -5; i <= 50; i++){
+//     console.log(`${i} ${angkaPrima(i)}`);
+// }
