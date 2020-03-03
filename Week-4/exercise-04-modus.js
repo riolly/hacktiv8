26ecda75168ebab61dfcb270008675b2df437f2f
riolly <albert.r.cahyantara@gmail.com>
2020-03-03 21:55:37 +0700

    Soal

---
+++ Week-4/exercise-04-modus.js
@@ -0,0 +1,10 @@
+function cariModus(arr) {
+    // you can only write your code here!
+}
+
+// TEST CASES
+console.log(cariModus([10, 4, 5, 2, 4])); // 4
+console.log(cariModus([5, 10, 10, 6, 5])); // 5
+console.log(cariModus([10, 3, 1, 2, 5])); // -1
+console.log(cariModus([1, 2, 3, 3, 4, 5])); // 3
+console.log(cariModus([7, 7, 7, 7, 7])); // -1
