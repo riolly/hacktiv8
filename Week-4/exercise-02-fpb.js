62be7f537ccbcdb7e936a5a36699f4bf8c7d1d2a
riolly <albert.r.cahyantara@gmail.com>
2020-03-03 21:14:56 +0700

    Same factor

--- Week-4/exercise-02-fpb.js
+++ Week-4/exercise-02-fpb.js
@@ -1,17 +1,38 @@
 function fpb(angka1, angka2) {
     let faktorAngka1 = cariFaktor(angka1);
     let faktorAngka2 = cariFaktor(angka2);
+    
+
 }
 
 function cariFaktor(bilangan){
     let faktor = [];
-    for(var i = 1; i < bilangan -1; i++){
+    for(let i = 1; i < bilangan; i++){
         if (bilangan % (i+1) == 0){
             faktor.push(i+1);
         }
     }
     return faktor;
 }
+
+function faktorSama(array1, array2){
+    let sama = [];
+    for (let i = 0; i < array1.length; i++){
+        for (let j = 0; j < array2.length; j++){
+            if (array1[i] == array2[j]){
+                sama.push(array1[i]);
+            }
+        }
+    }
+    return sama;
+}
+
+let faktor1 = cariFaktor(36);
+console.log(faktor1);
+let faktor2 = cariFaktor(64);
+console.log(faktor2);
+let sama = faktorSama(faktor1, faktor2)
+console.log(sama);
 
 // TEST CASES
 // console.log(fpb(12, 16)); // 4
