b41fe9d83a2b15335a31667226e8332eee628559
riolly <albert.r.cahyantara@gmail.com>
2020-03-04 11:49:25 +0700

    Menyusun ulang kata berdasarkan urutan karakternya

--- Week-4/exercise-07-urut-abjad.js
+++ Week-4/exercise-07-urut-abjad.js
@@ -1,5 +1,32 @@
 function urutkanAbjad(str) {
-    // you can only write your code here!
+    let array = [];
+    for (var g = 0; g < str.length; g++) {
+        array[g] = str[g];
+    }
+    // console.log(string);
+
+    for (var h = 0; h < array.length -1; h++) {
+        // console.log('\nIterasi ke '+h);
+        for (var i = 0; i < array.length -h -1; i++) {
+            // console.log(i);
+            if (array[i] > array[i+1]) {
+                // console.log(`${string[i]} > ${string[i+1]}`);
+                let buffer = array[i];
+                array[i] = array[i+1];
+                array[i+1] = buffer;
+                // console.log(string);
+            } else {
+                // console.log(`${string[i]} <= ${string[i+1]}`);
+            }
+        }
+    }
+
+    let backString = '';
+    for (var j = 0; j < array.length; j++) {
+        backString += array[j];
+    }
+    return backString;
+
 }
   
 // TEST CASES
