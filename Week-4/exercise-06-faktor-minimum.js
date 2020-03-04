deb69238c2b3e9441e376b4562d3fc36cfa76eda
riolly <albert.r.cahyantara@gmail.com>
2020-03-04 11:12:59 +0700

    Cek jumlah digit dari faktor berpasangan

--- Week-4/exercise-06-faktor-minimum.js
+++ Week-4/exercise-06-faktor-minimum.js
@@ -1,6 +1,29 @@
 function digitPerkalianMinimum(angka) {
-    // you can only write your code here!
+    const faktor = cariFaktor(angka);
+    let minDigit = 1 + String(faktor[faktor.length-1]).length;
+    for (var i = 1; i < Math.ceil(faktor.length/2); i++) {
+        let nextDigit = String(faktor[i]).length + String(faktor[faktor.length-1-i]).length;
+        if (nextDigit < minDigit) {
+            minDigit = nextDigit; 
+        }
+    }
+    return minDigit;
 }
+
+function cariFaktor(bilangan){
+    let faktor = [];
+    for(let i = 0; i < bilangan; i++){
+        if (bilangan % (i+1) == 0){
+            faktor.push(i+1);
+        }
+    }
+    return faktor;
+}
+// digitPerkalianMinimum(16);
+// console.log(cariFaktor(90));
+// console.log(cariFaktor(20));
+// console.log(cariFaktor(179));
+// console.log(cariFaktor(1));
 
 // TEST CASES
 console.log(digitPerkalianMinimum(24)); // 2
