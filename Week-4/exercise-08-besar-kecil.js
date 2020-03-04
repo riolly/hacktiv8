f1fd93ee23c86c701b6424c64eafde0996d9fdcd
riolly <albert.r.cahyantara@gmail.com>
2020-03-04 12:20:48 +0700

    Switching uppercase & lowercase

--- Week-4/exercise-08-besar-kecil.js
+++ Week-4/exercise-08-besar-kecil.js
@@ -1,5 +1,43 @@
 function tukarBesarKecil(kalimat) {
-    // you can only write your code here!
+    const alfabet = [
+        ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+        ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    ];
+    
+    let kalimatArray = [];                              // Pindah ke kalimat ke array baru
+    for (var h = 0; h < kalimat.length; h++) {
+        kalimatArray[h] = kalimat[h];
+    }
+
+    for (var i = 0; i < kalimatArray.length; i++) {
+        let j = 0;
+        let baris = 0;
+        while (true) {                                  // Cek karakter dengan setiap karakter dalam alfabet.
+            if (kalimatArray[i] != alfabet[baris][j]) {
+                j++;
+                if (baris == 0 && j == 26) {            // Jika baris lowercase tidak ada, ganti baris yang capital.
+                    baris = 1;
+                    j = 0;
+                } else if (baris == 1 && j == 26) {     // Jika tidak ada dalam array (simbol atau angka), karakter tidak berubah.
+                    break;
+                }
+            } else {
+                if (baris == 0){
+                    kalimatArray[i] = alfabet[1][j];    // bawah ganti atas
+                    break;
+                } else {
+                    kalimatArray[i] = alfabet[0][j];    // atas ganti bawah
+                    break;
+                }
+            }
+        }
+    }
+    
+    let kalimatString = '';                             // Kembalikan ke string
+    for (var k = 0; k < kalimatArray.length; k++) { 
+        kalimatString += kalimatArray[k];
+    }
+    return kalimatString;
 }
   
 // TEST CASES
