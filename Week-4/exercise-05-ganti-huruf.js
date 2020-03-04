9895bf8d92b6f82f34d1835c0b4e64f5de922040
riolly <albert.r.cahyantara@gmail.com>
2020-03-04 19:06:24 +0700

    Documentation

--- Week-4/exercise-05-ganti-huruf.js
+++ Week-4/exercise-05-ganti-huruf.js
@@ -1,19 +1,28 @@
+/**
+ * This fucntion change every character of a string the next one.
+ * @param {string} kata input kata
+ * 
+ * @link https://github.com/riolly
+ * @author Alberto Riolly <alberto.cahyantara@gmail.com>
+ */
 function ubahHuruf(kata) {
-    const alfabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a'];
+    const alfabet = "abcdefghijklmnopqrstuvwxyza";
     let kataBaru = '';
     for (var i = 0; i < kata.length; i++) {
         let j = 0;
-        while (kata[i] != alfabet[j]) {
+        while (kata[i] != alfabet[j]) {             // Check character until match the alphabet.
             j++;
         }
-        kataBaru += alfabet[j+1];
+        kataBaru += alfabet[j+1];                   // Assign the next alphabet to it.
     }
     return kataBaru;
 }
 
 // TEST CASES
-console.log(ubahHuruf('wow')); // xpx
-console.log(ubahHuruf('developer')); // efwfmpqfs
-console.log(ubahHuruf('javascript')); // kbwbtdsjqu
-console.log(ubahHuruf('keren')); // lfsfo
-console.log(ubahHuruf('semangat')); // tfnbohbu
+console.log(ubahHuruf('wow'));          // xpx
+console.log(ubahHuruf('developer'));    // efwfmpqfs
+console.log(ubahHuruf('javascript'));   // kbwbtdsjqu
+console.log(ubahHuruf('keren'));        // lfsfo
+console.log(ubahHuruf('semangat'));     // tfnbohbu
+
+console.log("\nIf there is a way to improve this code, please tell me :)");
