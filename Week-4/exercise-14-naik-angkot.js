df38717d35cf4c524420783f277a196357566c1e
riolly <albert.r.cahyantara@gmail.com>
2020-03-05 17:32:44 +0700

    Trip report

--- Week-4/exercise-14-naik-angkot.js
+++ Week-4/exercise-14-naik-angkot.js
@@ -1,8 +1,31 @@
+/**
+ * This function make a report of trip including, name, origin, destination, and fare.
+ * @param {Array} arrPenumpang Multidimention array with [name, origin, destination]
+ */
 function naikAngkot(arrPenumpang) {
-    rute = ['A', 'B', 'C', 'D', 'E', 'F'];
-    //your code here
+    var rute = ['A', 'B', 'C', 'D', 'E', 'F'];
+    var journeys = [];
+    for (let i = 0; i < arrPenumpang.length; i++) {
+        var trip = { // Make the object of trip report
+            penumpang: arrPenumpang[i][0],
+            naikDari: arrPenumpang[i][1],
+            tujuan: arrPenumpang[i][2],
+            biaya: 0
+        }
+        let awal = 0; // Find the starting point index
+        while (trip.naikDari != rute[awal]) {
+            awal++;
+        }
+        let akhir = 0; // Find the destination index
+        while (trip.tujuan != rute[akhir]) {
+            akhir++;
+        }
+        trip.biaya = (akhir - awal) * 2000; // Calculate the fare from distance
+        journeys.push(trip);
+    }
+    return journeys;
 }
-  
+
 //TEST CASE
 console.log(naikAngkot([['Dimitri', 'B', 'F'], ['Icha', 'A', 'B']]));
 // [ { penumpang: 'Dimitri', naikDari: 'B', tujuan: 'F', bayar: 8000 },
