1ce1f1f0cdab0acbd1836a4de2438a4ff04dfa18
riolly <albert.r.cahyantara@gmail.com>
2020-03-05 14:51:08 +0700

    Buy somethings if you have money

--- Week-4/exercise-11-shopping-time.js
+++ Week-4/exercise-11-shopping-time.js
@@ -1,5 +1,35 @@
+/**
+ * This function find what item can be buy using the money it has.
+ * @param {string} memberId 
+ * @param {number} money 
+ * @link https://github.com/riolly
+ * @author Alberto Riolly <alberto.cahyantara@gmail.com>
+ */
 function shoppingTime(memberId, money) {
-    // you can only write your code here!
+    const barangNama = ['Sepatu Stacattu', 'Baju Zoro', 'Baju H&N', 'Sweeter Uniklooh', 'Casing Handphone'];
+    const barangHarga = [1500000, 500000, 250000, 175000, 50000];
+    // Make the object
+    var shoppingChart = {                                           
+        memberId: memberId,
+        money: money,
+        listPurcashed: [],
+        changeMoney: money
+    }
+    // Check if the remaining money can be used to buy somethings
+    for (var i = 0; i < barangHarga.length; i++) {
+        if (shoppingChart.changeMoney >= barangHarga[i]) {
+            shoppingChart.listPurcashed.push(barangNama[i]);
+            shoppingChart.changeMoney -= barangHarga[i];
+        }
+    }
+    // Check if it has memberID and the money is enough
+    if (shoppingChart.memberId == '' || shoppingChart.memberId == undefined) {
+        return "Mohon maaf, toko X hanya berlaku untuk member saja";
+    } else if (shoppingChart.listPurcashed.length == 0) {
+        return "Mohon maaf, uang tidak cukup";
+    } else {
+        return shoppingChart;
+    }
 }
   
 // TEST CASES
@@ -25,3 +55,5 @@ console.log(shoppingTime('234JdhweRxa53', 15000));
 //Mohon maaf, uang tidak cukup
 console.log(shoppingTime()); 
 //Mohon maaf, toko X hanya berlaku untuk member saja
+
+console.log("\nIf there is a way to improve this code, please tell me :)");
