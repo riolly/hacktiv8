2430c569f3341659344aed50fdcf0c7d3204bd9e
riolly <albert.r.cahyantara@gmail.com>
2020-03-05 16:26:14 +0700

    Process purchasing

--- Week-4/exercise-12-toko-x.js
+++ Week-4/exercise-12-toko-x.js
@@ -1,12 +1,59 @@
+/**
+ * This function process purchasing, update stock item, and profit.
+ * @example
+ * const purchase = [{name: 'Windi', product: 'Sepatu Stacattu', amount: 2}, ...]
+ * countProfit(purchase);
+ * @param {object[]} shoppers name, item, amount
+ * @link https://github.com/riolly
+ * @author Alberto Riolly <alberto.cahyantara@gmail.com>
+ */
 function countProfit(shoppers) {
-    let listBarang = [ ['Sepatu Stacattu', 1500000, 10],
-                       ['Baju Zoro', 500000, 2],
-                       ['Sweater Uniklooh', 175000, 1]
-                     ];
-  
-    // you can only write your code here!
+  let listBarang = [
+    ['Sepatu Stacattu', 1500000, 10],
+    ['Baju Zoro', 500000, 2],
+    ['Sweater Uniklooh', 175000, 1]
+  ];
+  if (shoppers.length == 0) {
+    return [];
+  }
+  // Create all product object
+  var stockBarang = [];
+  for (var i = 0; i < listBarang.length; i++) {
+    var Pembelian = {
+      product: listBarang[i][0],
+      shoppers: [],
+      leftOver: listBarang[i][2],
+      totalProfit: 0
+    }
+    stockBarang.push(Pembelian);
+  }
+  // Process each purchase
+  for (var j = 0; j < shoppers.length; j++) {
+    // Check the product that bought
+    let indexBarang;
+    switch (shoppers[j].product) {
+      case 'Sepatu Stacattu':
+        indexBarang = 0;
+        break;
+      case 'Baju Zoro':
+        indexBarang = 1;
+        break;
+      case 'Sweater Uniklooh':
+        indexBarang = 2;
+        break;
+    }
+    // If the product doesn't match will all product in stock
+    if (indexBarang == undefined) {break;}
+    // Update buyer, stock, & profit
+    if (shoppers[j].amount <= stockBarang[indexBarang].leftOver) {
+      stockBarang[indexBarang].shoppers.push(shoppers[j].name);
+      stockBarang[indexBarang].leftOver -= shoppers[j].amount;
+      stockBarang[indexBarang].totalProfit += shoppers[j].amount * listBarang[indexBarang][1];
+    }
+  }
+  return stockBarang;
 }
-  
+
 // TEST CASES
 console.log(countProfit([{name: 'Windi', product: 'Sepatu Stacattu', amount: 2}, {name: 'Vanessa', product: 'Sepatu Stacattu', amount: 3}, {name: 'Rani', product: 'Sweater Uniklooh', amount: 2}]));
 //[ { product: 'Sepatu Stacattu',
@@ -49,3 +96,5 @@ console.log(countProfit([{name: 'Windi', product: 'Sepatu Naiki', amount: 5}]));
 //     totalProfit: 0 } ]
 console.log(countProfit([]));
 //[]
+
+console.log("\nIf there is a way to improve this code, please tell me :)");
