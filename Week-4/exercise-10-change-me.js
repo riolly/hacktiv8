cf84498479a2c76b012832d767346fa91ef3dd0a
riolly <albert.r.cahyantara@gmail.com>
2020-03-05 10:55:58 +0700

    Create person object from 2d array using function constructor

--- Week-4/exercise-10-change-me.js
+++ Week-4/exercise-10-change-me.js
@@ -1,5 +1,28 @@
+/**
+ * This function create new 2D array of personal data into an object and print it.
+ * @param {Array} arr input array of mutiple person data;
+ */
 function changeMe(arr) {
-    // you can only write your code here!
+    nowYear = 2020;
+    for (var i = 0; i < arr.length; i++) {
+        var orang = new Person(arr[i]);
+        console.log(`${i+1}. ${orang.firstName} ${orang.lastName}:`)
+        console.log(orang);
+    }
+}
+/**
+ * This function create new person object from the input array.
+ * @param {Array} arr input array
+ */
+function Person(arr){
+    this.firstName = arr[0];
+    this.lastName = arr[1];
+    this.gender = arr[2];
+    if (arr[3] == undefined || arr[3] > nowYear) {
+        this.age = 'Invalid';
+    } else {
+        this.age = nowYear - arr[3];
+    }
 }
 
 // TEST CASES
@@ -16,3 +39,5 @@ changeMe([['Christ', 'Evans', 'Male', 1982], ['Robert', 'Downey', 'Male']]);
 //   age: 'Invalid Birth Year' }
 changeMe([]);
 // ""
+
+console.log("\nIf there is a way to improve this code, please tell me :)");
